b27123baee512f6264922376e79a6d481ad6594b
Svetlana1234567891011 <153299652+Svetlana1234567891011@users.noreply.github.com>
2024-06-19 16:00:30 +0300

    Update test_create_user.py

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -1,23 +1,19 @@
 import pytest
 import allure
-from utils.special_request import UserRequests, generate_random_string
+from utils.special_request import UserRequests, fake
 
-randoms_string = generate_random_string(10)
+
 
 
 @allure.feature('Создание Пользователя')
 class TestCreateCourier:
 
     @allure.title('Можно создать Пользователя со случайным логином')
-    @pytest.mark.parametrize('login_v, password_v, firstname_v',
-                             [
-                                 (randoms_string, randoms_string, randoms_string),
-                             ])
-    def test_all_the_fields_are_required(self, login_v, password_v, firstname_v):
+    def test_all_the_fields_are_required(self, generate_random_string_10):
         payload = {
-            "login": login_v,
-            "password": password_v,
-            "firstName": firstname_v
+            "login": generate_random_string_10,
+            "password": generate_random_string_10,
+            "firstName": generate_random_string_10
         }
 
         response = UserRequests().post_create_user(data=payload)
@@ -29,20 +25,30 @@ class TestCreateCourier:
             data=courier_with_payload.payload)  # отправляем данные повторно
         assert response_dupe["message"] == "User already exists"
 
-    @pytest.mark.parametrize('login_v, password_v, firstname_v',
+    @pytest.mark.parametrize('login_v, password_v, email_v',
                              [
-                                 (None, randoms_string, randoms_string),
-                                 (None, randoms_string, None),
-                                 (randoms_string, None, None),
-                                 (None, None, randoms_string),
+                                 (None, fake.password(), fake.email()),
+                                 (fake.name(), fake.password(), None),
+                                 (fake.name(), None, fake.email()),
 
                              ])
     @allure.title('Для создания Пользователя необходимо задать все обязательные поля (логин, пароль)')
-    def test_all_the_fields_are_required(self, login_v, password_v, firstname_v):
+    def test_all_the_fields_are_required(self, login_v, password_v, email_v):
         payload = {
-            "login": login_v,
+            "name": login_v,
             "password": password_v,
-            "firstName": firstname_v
+            "email": email_v
+        }
+
+        response = UserRequests().post_create_user(data=payload)
+        assert response["message"] == "Email, password and name are required fields"
+
+    @allure.title('Нельзя создать пользователя, не заполнив одно из обязательных полей - например, имейл')
+    def test_email_field_are_required(self, generate_random_string_10):
+        payload = {
+            "login": generate_random_string_10,
+            "password": generate_random_string_10,
+            "email": None
         }
 
         response = UserRequests().post_create_user(data=payload)
